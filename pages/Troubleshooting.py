ad8987433d149139635c4d74d637c9922ffaa12e
AidanMaddock <165080013+AidanMaddock@users.noreply.github.com>
2025-08-22 22:29:36 +0000

    Updated comparison logic

---
+++ pages/Troubleshooting.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import sys, os
+import pandas as pd 
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
+
+dfex = pd.read_csv("example_data.csv")
+
+st.write("CSV must contain columns in order as below:")
+st.dataframe(dfex.set_index(dfex.columns[0]))
+st.write("Make sure that date is in format DD/MM/YYYY and that numeric values are not stored as text or include extra spaces.")
--- pages/streamlit_app.py
+++ pages/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Main page with navigation set in Streamlit
-pg = st.navigation([st.Page("Cross_Section.py"), st.Page("Comparison.py")],position="top"
+pg = st.navigation([st.Page("Cross_Section.py"), st.Page("Comparison.py"), st.Page("Troubleshooting.py")],position="top"
 )
 pg.run()
 
